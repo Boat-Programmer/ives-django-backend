d4e18f51954c319add9c6d20f16058f5bfbdbea1
Boat-Programmer <53931555+Boat-Programmer@users.noreply.github.com>
2020-11-25 23:55:18 +0700

    upp

--- DjangoMedicalStoreManagementSystem/settings.py
+++ DjangoMedicalStoreManagementSystem/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -150,8 +150,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://ives-django-backend.herokuapp.com",
 ]
